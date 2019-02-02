b71c78e4f17e5c3b1ab84079b0141e3dde770a7b
jaebooker <jaesonbooker@gmail.com>
2019-02-02 15:50:03 -0800

    cleaned up a bit

--- echo_project/src/main/main.go
+++ echo_project/src/main/main.go
@@ -5,11 +5,12 @@ import(
     "net/http"
     "github.com/labstack/echo"
 )
+func greetingWeb(c echo.Context) error {
+    return c.String(http.StatusOK, "Hello from the sever side")
+}
 func main() {
     fmt.Printf("Mornin', Starshine!")
     e := echo.New()
-    e.GET("/", func (c echo.Context) error {
-        return c.String(http.StatusOK, "Hello from the sever side")
-    })
+    e.GET("/", greetingWeb)
     e.Start(":8000")
 }
