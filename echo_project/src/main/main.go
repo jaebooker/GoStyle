43cf548dabaaf147cfe4133191138131a6f81cf1
jaebooker <jaesonbooker@gmail.com>
2019-02-02 16:13:46 -0800

    added params, baby steps

--- echo_project/src/main/main.go
+++ echo_project/src/main/main.go
@@ -8,9 +8,15 @@ import(
 func greetingWeb(c echo.Context) error {
     return c.String(http.StatusOK, "Hello from the sever side")
 }
+func getCats(c echo.Context) error {
+    catName := c.QueryParam("catname")
+    catType := c.QueryParam("catspecies")
+    return c.String(http.StatusOK, fmt.Sprintf("Your cat's name is %s\nand she or he's a %s\n", catName, catType))
+}
 func main() {
     fmt.Printf("Mornin', Starshine!")
     e := echo.New()
     e.GET("/", greetingWeb)
+    e.GET("/cats", getCats)
     e.Start(":8000")
 }
