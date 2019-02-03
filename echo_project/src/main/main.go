1919daf672124046198a97f55cdd26bf111240e2
jaebooker <jaesonbooker@gmail.com>
2019-02-02 17:51:10 -0800

    json post created

--- echo_project/src/main/main.go
+++ echo_project/src/main/main.go
@@ -3,8 +3,15 @@ package main
 import(
     "fmt"
     "net/http"
+    "io/ioutil"
+    "log"
+    "encoding/json"
     "github.com/labstack/echo"
 )
+type Cat struct {
+    Name    string  `json:"name"`
+    Type    string  `json:"type"`
+}
 func greetingWeb(c echo.Context) error {
     return c.String(http.StatusOK, "Hello from the sever side")
 }
@@ -25,10 +32,27 @@ func getCats(c echo.Context) error {
         "error": "this is wrong",
     })
 }
+func addKittyCat(c echo.Context) error {
+    cat := Cat{}
+    defer c.Request().Body.Close()
+    b, err := ioutil.ReadAll(c.Request().Body)
+    if err != nil {
+        log.Printf("Failed reading: %s", err)
+        return c.String(http.StatusInternalServerError, "")
+    }
+    err = json.Unmarshal(b, &cat)
+    if err != nil {
+        log.Printf("Failed inmarshalling: %s", err)
+        return c.String(http.StatusInternalServerError, "")
+    }
+    log.Printf("Here is your kitty cat, Sir:", cat)
+    return c.String(http.StatusOK, "We have received your kitty cat, Sir.")
+}
 func main() {
     fmt.Printf("Mornin', Starshine!")
     e := echo.New()
     e.GET("/", greetingWeb)
     e.GET("/cats/:data", getCats)
+    e.POST("/cats", addKittyCat)
     e.Start(":8000")
 }
