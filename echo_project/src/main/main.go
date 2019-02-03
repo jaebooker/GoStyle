779c53f240a3e654d066666d2c1dfee1ccadc11b
jaebooker <jaesonbooker@gmail.com>
2019-02-03 02:37:26 -0800

    adding basic authentication

--- echo_project/src/main/main.go
+++ echo_project/src/main/main.go
@@ -98,6 +98,12 @@ func main() {
     g.Use(middleware.LoggerWithConfig(middleware.LoggerConfig{
             Format: `[${time_rfc3339}] ${status} ${method} ${host}${path} ${latency_human}` + "\n",
     }))
+    g.Use(middleware.BasicAuth(func(username, password string, c echo.Context) (bool, error) {
+        if username == "PiereKirby" && password == "1234" {
+            return true, nil
+        }
+        return false, nil
+    }))
     g.GET("/main", mainAdmin, middleware.Logger())
     e.GET("/", greetingWeb)
     e.GET("/cats/:data", getCats)
